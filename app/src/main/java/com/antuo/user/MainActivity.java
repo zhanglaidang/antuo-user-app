2f2e8660308aef7062547285caaa2f7179b478be
zhanglaidang <738680069@qq.com>
2026-03-18 02:22:17 +0800

    Fix: Add missing Java imports (IOException)

--- app/src/main/java/com/antuo/user/MainActivity.java
+++ app/src/main/java/com/antuo/user/MainActivity.java
@@ -33,6 +33,7 @@ import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
